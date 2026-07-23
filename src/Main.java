0d7b77f59cf36749072a0162dbb250c9701e07ec
tejawini <tejawinirakhunde06@gmail.com>
2026-07-23 23:09:38 +0530

    uppercase or lowercase

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,67 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*public class Main {
+    public static void main ( String[] args){
+        System.out.println("Hello World");
+    }
+} */
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        /*System.out.println("Enter Your Name : ");
+        String name = sc.nextLine();
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter your city: ");
+        String city = sc.nextLine();
+        System.out.println("My name is " + name + ".My age is " + age + ".I live in "+ city);
+        sc.close();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int b = sc.nextInt();
+        int sum = a + b ;
+        if (a > b){
+            int diff = a - b ;
+            System.out.println("Difference of "+ a + " - " + b + " is " + diff);
         }
+        else {
+            int diff = b - a ;
+            System.out.println("Difference of "+ b + " - " + a + " is " + diff);
+        }
+        int diff = a - b ;
+        System.out.println("Difference of "+ a + " - " + b + " is " + diff);
+        int mul = a * b ;
+        System.out.println("Multiplication of "+ a + " x " + b + " is " + mul);
+        try {
+            int div = a / b;
+            System.out.println("Division of"+a+" / " + b + " is "+div);
+
+        }
+        catch(Exception f){
+            System.out.println(f);
+        }
+        int rem = a % b ;
+        System.out.println("Remainder : " + rem);
+        System.out.println("Before Swapping");
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+        int c = a ;
+        a = b;
+        b = c;
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("After Swapping");
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+        System.out.print("Enter temperature in celsius : ");
+        int a = sc.nextInt();
+        int b = ((a * 9) / 5 ) + 32;
+        System.out.println("temperature in farehnite: " +b);*/
+
     }
 }
--- src/Operator.java
+++ src/Operator.java
@@ -1,4 +1,73 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class Operator {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        /*System.out.print("Enter the year: ");
+        int a = sc.nextInt();*/
+        System.out.print("Enter the character: ");
+        char b = sc.next().charAt(0);
+        /*System.out.print("Enter the third number: ");
+        int c = sc.nextInt();
+        if (a%2 == 0){
+            System.out.println(a + " is even number.");
+        }
+        else{
+            System.out.println(a + " is a odd number.");
+        }
+        if (a == 0){
+            System.out.println("number is zero");
+        }
+        else if(a > 0){
+            System.out.println(a+" is positive");
+        }
+        else{
+            System.out.println(a+" number is negative");
+        }
+        if (a>b && a > c){
+            System.out.println(a + " is a largest number.");
+        }
+        else if(b > a && b > c){
+            System.out.println(b + " ia a largest number.");
+        }
+        else{
+            System.out.println(c + " ia a largest number.");
+        }
+        if ((a % 400) == 0 ||((a %4) == 0 && (a% 100 != 0) )){
+            System.out.println(a + " is a leap year");
+        }
+        else {
+            System.out.println(a + " is not a leap year");
+        }
+        System.out.print("Enter your character: ");
+        char a = sc.next().charAt(0);
+        if ( (a == 'a') || (a == 'e') ||(a == 'i') ||(a == 'o')
+                ||(a == 'u') ||(a == 'A') ||(a == 'E') ||(a == 'I') ||(a == 'O') ||(a == 'U') ){
+            System.out.println(a + " is a vowel.");
+        }
+        else{
+            System.out.println(a + " is a consonant");
+        }
+        if (b < 0){
+            int r = b * -1 ;
+            System.out.println(r);
+        }
+        else{
+            System.out.println(b);
+        }
+        if ( (b%5 == 0) && (b%11 == 0)){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+        if (b >= 'A' && b <= 'Z'){
+            System.out.println(b + " is a uppercase letter.");
+        }
+        else if (b >= 'a' && b <= 'z'){
+            System.out.println(b + " is a lowercase letter.");
+        }
+        else{
+            System.out.println("Invalid");
+        }*/
+    }
 }
