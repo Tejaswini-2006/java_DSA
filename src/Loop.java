898417367eca0fe46ab0919b4af397a7ab47039f
tejawini <tejawinirakhunde06@gmail.com>
2026-07-24 22:59:19 +0530

    multiplication

--- src/Loop.java
+++ src/Loop.java
@@ -2,14 +2,17 @@ import java.util.*;
 public class Loop {
     public static void main(String[] args ){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of n: ");
+        System.out.print("Enter the value of n: ");
         int  n = sc.nextInt();
-        int s= 0 ;
+        for (int i = 1; i < 11 ; i++){
+            System.out.println(n+" x " + i + " = " + (n*i));
+        }
+        /*int s= 0 ;
        for (int i = 0 ; i <= n ; i++){
            s = s + i ;
        }
         System.out.println((s));
-        /*for (int i = 1; i <=100;i++ ){
+        for (int i = 1; i <=100;i++ ){
             if (i %2 != 0){
                 System.out.println(i);
             }
