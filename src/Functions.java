ac220eb7b06bc6414c46d0180a970fb7f61ddb91
tejawini <tejawinirakhunde06@gmail.com>
2026-08-18 18:16:46 +0530

    greater no in function

--- src/Functions.java
+++ src/Functions.java
@@ -14,7 +14,7 @@ public class Functions {
         int c = sc.nextInt();
         System.out.println(average(a,b,c));
     }*/
-    public static int SumOfOdd(int n){
+    /*public static int SumOfOdd(int n){
         int sum = 0 ;
         for(int i = 1 ; i <= n ; i++){
             if(i % 2 != 0){
@@ -29,5 +29,19 @@ public class Functions {
         int n = sc.nextInt();
         System.out.println("Sum of all odd numbers from 1 to " + n + " is " + SumOfOdd(n));
 
+    }*/
+    public static void GreaterNumber(int a , int b){
+        if ( a > b)
+            System.out.println(a + " is greater than " + b);
+        else
+            System.out.println(b + " is greater than " + a);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 1st number: ");
+        int x = sc.nextInt();
+        System.out.println("Enter the 2nd number: ");
+        int y = sc.nextInt();
+        GreaterNumber(x,y);
     }
 }
